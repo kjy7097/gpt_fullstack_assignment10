2bd23f8161d4b3704bd4b2ba53cb2fda6ae502d8
jy <kjy7097@gmail.com>
2025-01-26 14:56:14 +0900

    Remove unused binary file downloader function from app.py

--- app.py
+++ app.py
@@ -30,14 +30,6 @@ with st.sidebar:
         st.cache_data.clear()
 
 
-def get_binary_file_downloader_html(bin_file, file_label="File"):
-    with open(bin_file, "rb") as f:
-        data = f.read()
-    bin_str = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download a demo {file_label}</a>'
-    return href
-
-
 if api_key:
     util = Util()
     util.create_assistant()
